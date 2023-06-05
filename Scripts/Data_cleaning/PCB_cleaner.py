58416a695ad7bd9b6de85b0ec97000951d88290d
ybiao-he <ybiao2016@gmail.com>
2023-06-05 13:10:13 +0800

    refactor data cleaning code and update readme

--- Scripts/Data_cleaning/PCB_cleaner.py
+++ Scripts/Data_cleaning/PCB_cleaner.py
@@ -1,209 +1,86 @@
+import csv
+from functools import reduce
+from collections import defaultdict
 import sys
 sys.path.append('..')
+from Data_extraction.thirdparty.kicad_parser.kicad_pcb import *
 
-from typing import Any, Dict, List, Set, Tuple
-from Data_extraction.extract_kicad import PCB
-from collections import defaultdict
-import math
-import networkx as nx
+import os
 
 
-PAD_NODE_DIST_THRESHOLD = 0.001
 
-def PCB_cleaner(
-        pcb: PCB, 
-        clean_segments: bool=True, 
-        clean_vias: bool=True, 
-        clean_pads: bool=True
-    ) -> None:
-    
-    if clean_segments:
-        del_incomplete_segments(pcb)
+def read_csv(file_name):
+    with open(file_name, "r") as f:
+        pcb_names = list(csv.reader(f, delimiter=","))
 
-    if clean_vias:
-        del_unconnected_vias(pcb)
-
-    if clean_pads:
-        del_isolate_pads(pcb)
-
-def del_incomplete_segments(pcb: PCB) -> None:
-    """ delete all segments for a net if its connections use fill zones or there are some incomplete connections
-    """
-    # extract net segments
-    net2segs = defaultdict(list)
-    for seg in pcb.wires:
-        layer_idx = pcb.layers.index(seg.layer)
-        net2segs[seg.net].append([tuple(seg.start + [layer_idx]), tuple(seg.end + [layer_idx])])
-    # need to treat vias as a segment to create a graph for wires/paths
-    for via in pcb.vias:
-        l1, l2 = pcb.layers.index(via.layers[0]), pcb.layers.index(via.layers[1])
-        ls, le = min(l1, l2), max(l1, l2)
-        for l in range(ls, le):
-            net2segs[via.net].append([tuple(via.at + [l]), tuple(via.at + [l+1])])
-
-    net2seggraph = dict()
-    for net, segs in net2segs.items():
-        # convert a net segments into a graph
-        if net not in pcb.net_pads:
-            net2seggraph[net] = []
-            continue
-        seg_graph = defaultdict(list)
-        for seg in segs:
-            if seg[0] != seg[1]:
-                seg_graph[seg[0]].append(seg[1])
-                seg_graph[seg[1]].append(seg[0])
-
-        # delete unconnected segments/edges from graph
-        for node in seg_graph:
-            curr_node = node
-            while len(seg_graph[curr_node]) == 1 and not is_pad_node(curr_node, pcb.net_pads[net], pcb.layers):
-                next_node = seg_graph[curr_node].pop()
-                seg_graph[next_node].pop(seg_graph[next_node].index(curr_node))
-                curr_node = next_node
-
-        all_nodes = list(seg_graph.keys())
-        for node in all_nodes:
-            if len(seg_graph[node]) == 0 and not is_pad_node(node, pcb.net_pads[net], pcb.layers):
-                del seg_graph[node]
-        # check if the cleaned graph is fully connected, if not, delete this net
-        if is_graph_fully_connected(seg_graph):
-            net2seggraph[net] = seg_graph
-        else:
-            net2seggraph[net] = []
-    
-    new_segments = []
-    for seg in pcb.wires:
-        start_node = tuple(seg.start + [pcb.layers.index(seg.layer)])
-        end_node = tuple(seg.end + [pcb.layers.index(seg.layer)])
-        net_idx = seg.net
-        if len(net2seggraph[net_idx]) > 0 and len(net2seggraph[net_idx][start_node]) > 0 and len(net2seggraph[net_idx][end_node]) > 0:
-            new_segments.append(seg)
-
-    pcb.wires = new_segments
+    pcb_names = reduce(lambda xs, ys: xs + ys, pcb_names)
+    return pcb_names
 
 
-def del_unconnected_vias(pcb: PCB) -> None:
-    """ If a via is not connected by any given paths/wires of the given routed circuits, 
-        then this via is viewed as an unconnected one and deleted.
-    """
-    seg_nodes = get_segment_end_nodes(pcb.wires, pcb.layers)
-    new_vias = []
-    for via in pcb.vias:
-        l1, l2 = pcb.layers.index(via.layers[0]), pcb.layers.index(via.layers[1])
-        ls, le = min(l1, l2), max(l1, l2)
-        for l in range(ls, le+1):
-            via_node_l = tuple(via.at + [l])
-            if via_node_l in seg_nodes:
-                new_vias.append(via)
-                break
+def revise_module_fp_text(pcb):
+    text_count = defaultdict(int)
+    for m in pcb.module:
+        text_count[m.fp_text[0][1]] += 1
 
-    pcb.vias = new_vias
-
-
-def del_isolate_pads(pcb: PCB) -> None:
-    """ If a pad/pin is not connected by any given paths/wires of the given routed circuits, 
-        then this pad/pin is viewed as an isolated one and deleted.
-    """
-    seg_nodes = get_segment_end_nodes(pcb.wires, pcb.layers)
-    obs_pads = []
-    for net, pads in pcb.net_pads.items():
-        new_pads = []
-        drill_holes = dict()
-        single_layer_pads = []
-        for pad_info in pads:
-            if pad_info["drill_hole"]:
-                drill_holes[pad_info['m_p_index']] = pad_info
+    for m in pcb.module:
+        if text_count[m.fp_text[0][1]] > 1 or m.fp_text[0][1] == '""' or m.fp_text[0][1] == '" "':
+            tmp_text = m.fp_text[0][1]
+            if tmp_text == '""':
+                m.fp_text[0][1] =  f"Noname_{text_count[tmp_text]}"
+            elif tmp_text == '" "':
+                m.fp_text[0][1] =  f"None_{text_count[tmp_text]}"
             else:
-                single_layer_pads.append(pad_info)
-        for pad_info in single_layer_pads:
-            pad_pos = pad_info["pad_center_xy"] + (pcb.layers.index(pad_info["pad_layer"]),)
-            if is_path_end_node(pad_pos, min(pad_info["pad_size"]), seg_nodes):
-                new_pads.append(pad_info)
-            else:
-                obs_pads.append(pad_info)
-        for _, dhs in drill_holes.items():
-            dh_obs = True
-            for l in dhs["pad_layer"]:
-                dh_pos = dhs["pad_center_xy"] + (pcb.layers.index(l),)
-                if is_path_end_node(dh_pos, min(pad_info["pad_size"]), seg_nodes):
-                    dh_obs = False
-            if dh_obs:
-                obs_pads.append(dhs)
-            else:
-                new_pads.append(dhs)
-
-        pcb.net_pads[net] = new_pads
-
-    if pcb.obs_pad_value not in pcb.net_pads:
-        pcb.net_pads[pcb.obs_pad_value] = []
-    pcb.net_pads[pcb.obs_pad_value] += obs_pads
+                m.fp_text[0][1] =  str(tmp_text) + "_" + str(text_count[tmp_text])
+            del m.fp_text[0][1][0]
+            text_count[tmp_text] -= 1
 
 
-def get_segment_end_nodes(segments: List[Dict[str, Any]], layers: List[str]) -> Set[Tuple[float, float, float]]:
-    # get all segment end nodes
-    seg_nodes = set()
-    for seg in segments:
-        layer_idx = layers.index(seg.layer)
-        start_node, end_node = tuple(seg.start + [layer_idx]), tuple(seg.end + [layer_idx])
-        seg_nodes.add(start_node)
-        seg_nodes.add(end_node)
+def delete_segment_via(pcb, delete_nets):
+    seg_idx = []
+    via_idx = []
+    for i in range(len(pcb.segment)):
+        if pcb.segment[i].net in delete_nets:
+            seg_idx.append(i)
+    seg_idx.sort()
+    for i in range(len(pcb.via)):
+        if pcb.via[i].net in delete_nets:
+            via_idx.append(i)
+    via_idx.sort()
 
-    return seg_nodes
+    bc = 0
+    for i in seg_idx:
+        del_i = i - bc
+        if pcb.segment[del_i].net in delete_nets:
+            del pcb.segment[del_i]
+            bc += 1
 
-
-def is_path_end_node(
-        node_xyz: Tuple[float, float, float], 
-        pad_size: float,
-        seg_nodes: Set[Tuple[float, float, float]]
-    ) -> bool:
-
-    for node in seg_nodes:
-        if node_xyz[-1] == node[-1] and math.dist([node[0], node[1]], [node_xyz[0], node_xyz[1]]) < PAD_NODE_DIST_THRESHOLD:
-            return True
-    return False
-
-
-def is_pad_node(
-        node_xyz: Tuple[float, float, float], 
-        net_pads: List[Dict[str, Any]], 
-        layers: List[str]
-    ) -> bool:
-    for pad_info in net_pads:
-        pad_layers = [layers.index(pl) for pl in pad_info["pad_layer"]]
-        if node_xyz[-1] not in pad_layers:
-            continue
-        pad_xy = pad_info["pad_center_xy"]
-        pad_size = min(pad_info["pad_size"])
-        dist = math.dist([node_xyz[0], node_xyz[1]], pad_xy)
-        if dist < PAD_NODE_DIST_THRESHOLD:
-            return True
-    
-    return False
+    bc = 0
+    for i in via_idx:
+        del_i = i - bc
+        if pcb.via[del_i].net in delete_nets:
+            del pcb.via[del_i]
+            bc += 1
 
 
-def is_graph_fully_connected(
-        graph: Dict[Tuple[float, float, float], List[Tuple[float, float, float]]],
-    ) -> bool:
+def delete_fill_zone(pcb_names):
+    fill_zone_folder = "Cleaning_PCBs"
+    if not os.path.exists(fill_zone_folder):
+        os.mkdir(fill_zone_folder)
 
-    edges = []
-    for node in graph:
-        for next_node in graph[node]:
-            edges.append([node, next_node])
-    if len(edges) == 0:
-        return False
-    G = nx.Graph()
-    G.add_edges_from(edges)
-    return nx.is_connected(G)
-
+    for name in pcb_names:
+        pcb_file_path = "../../PCBs/" + name + "/raw.kicad_pcb"
+        new_pcb_name = name + ".kicad_pcb"
+        pcb = KicadPCB.load(pcb_file_path)
+        fill_zone_nets = set([])
+        for zone in pcb.zone:
+            fill_zone_nets.add(zone.net)
+        del pcb.zone
+        delete_segment_via(pcb, fill_zone_nets)
+        revise_module_fp_text(pcb)
+        pcb.export(os.path.join(fill_zone_folder, new_pcb_name))
 
 if __name__ == "__main__":
-    import sys
-    kicad_filename = sys.argv[1]
 
-    pcb = PCB(kicad_filename)
-    PCB_cleaner(pcb=pcb)
-    del_pads = pcb.net_pads[pcb.obs_pad_value]
+    pcb_names = read_csv("clean_list.csv")
+    delete_fill_zone(pcb_names)
 
-    from dump_wires2kicad_segments import dump_wires
-
-    dump_wires(kicad_filename, pcb.wires, pcb.vias, del_pads)
--- Scripts/Data_cleaning/README.md
+++ Scripts/Data_cleaning/README.md
@@ -1,28 +1,24 @@
 # Data Cleaning
 
-## Dependencies
-1. numpy
+## Prerequisites
++ KiCAD 5
++ FreeRouting>=1.7
++ Python 3
 
-## Extract PCB information
-To extract PCB information, please call `PCB` in `extract_kicad.py`. Here is an example code:
-```
-kicad_filename = "./benchmarks/real_world/1bitsy.kicad_pcb"
-pcb = PCB(kicad_filename)
-```
-
-The extracted infomation are stored in `PCB` class, it includes following PCB inforamtion (look at [test.ipynb](https://github.com/ybiao-he/PCBench/blob/main/test.ipynb) about how to access each info):
-1. layers: a list of layer names
-2. boundary lines (now it only support straight lines, arcs need to be added later): a list of boundary lines
-3. circuit range: a list of min_x, min_y, max_x, max_y
-4. net info: a dict including: clearance, trace width, via diameter, vias drill diameter, microvia diameter, microvia drill diameter
-5. net pads: a dict including: center xy coordinate, pad vertices xy coordinates, pad size, pad layer, if the pad is drill hole
-6. wires: a list of wires
-7. vias: a list of vias
-
-## Clean up data
-Run `python PCB_cleaner.py kicad_file_path`. It will output a `processed.kicad_pcb` file to the folder of `kicad__file_path`.
-
-The current clean up includes:
-1. delete incomplete wires
-2. delete isolated vias
-3. convert isolated pad to obstacles
+## PCB cleaning
+1. Create `clean_list.csv`. List names of PCBs to be cleaned in `PCBs` folder in the `.csv` file named `clean_list.csv`, where each row contains one PCB name.  An example `clean_list.csv` is provided in this folder.
+2. Run `PCB_cleaner.py` to delete fill zones and adjust `fp_text` names in `.kicad_pcb` file.
+	```
+	python PCB_clean.py
+	```
+	This will generate a folder named `Cleaning_PCBs` containing all PCBs after deleting fill zones. In the folder, each PCB has a name `pcb_name.kicad_pcb` where `pcb_name` is the named listed in `clean_list.csv`.
+3. Use [FreeRouting](https://github.com/freerouting/freerouting) to route nets that are originally connected by fill zones. To do this, first open a `pcb_name.kicad_pcb` file in folder `Cleaning_PCBs` using KiCAD 5 and export it as `pcb_name.dsn` file. Then run FreeRouting by
+	```
+	java -jar freerouting-1.7.0.jar -de ./Cleaning_PCBs/pcb_name.dsn -do ./Cleaning_PCBs/pcb_name.ses
+	```  
+	After routing of FreeRouting, open `pcb_name.kicad_pcb` file in folder `Cleaning_PCBs` and import the `pcb_name.ses` and save `pcb_name.kicad_pcb`
+4. Update dataset in `PCBs` folder by runing
+	```
+	python update_dataset.py
+	```
+	This will move all the `.kicad_pcb` files in `Clean_PCBs` folder to the corresponding PCB folder in `PCBs` folder of the root. Note that after runing this update script, the `Cleaning_PCBs` will be deleted.
