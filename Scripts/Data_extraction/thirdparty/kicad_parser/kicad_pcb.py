962f5a457761434f04602a647a3f60cef455b35a
JacobFrieden <58039818+JFrieden@users.noreply.github.com>
2023-06-06 05:19:10 -0500

    possible v6 support

--- Scripts/Data_extraction/thirdparty/kicad_parser/kicad_pcb.py
+++ Scripts/Data_extraction/thirdparty/kicad_parser/kicad_pcb.py
@@ -8,6 +8,7 @@ it is to implement a parser in an almost declarative way.
 A usage demonstration is available in `test.py`
 '''
 
+import collections
 import json
 from .sexp_parser import *
 
@@ -86,15 +87,27 @@ class KicadPCB(SexpParser):
     @staticmethod
     def load(filename):
         ret = None
-        with open(filename,'r') as f:               # v5 or v6 
+        with open(filename,'r', encoding='utf-8') as f:               # v5 or v6 
             ret = KicadPCB(parseSexp(f.read()))
             version = ret.version
         if (version) > 20211000:  # if v6, extract net_class from project file. 
             circuitname = filename[0:len(filename)-10]   # assumes project file and pcb file have same name & in same dir
             try:
                 with open(circuitname+'.kicad_pro') as f:
-                    ret.net_class = json.load(f)['net_settings']['classes']
-                    ret.V6ToV5Naming()
+                    net_settings = json.load(f)['net_settings']
+                    for net_class in net_settings['classes']:
+                         net_class = V6ToV5Naming(net_class)
+                         ret.net_class._value.append(Sexp('net_class', collections.OrderedDict(net_class)))
+                    
+                    # In v6, classes other than Default have lists of member nets in kicad_pro
+                    # which we can convert to the add_net Sexp structure in V6ToV5Naming
+                    # afterward, all remaining nets (found in ret.net) are put into the add_net Sexp structure for
+                    # the Default net_class.
+                    for nc in ret.net_class:
+                        if nc[0] == "Default":
+                            for net in ret.net:
+                                if net[1] not in CLASSED_NETS:
+                                    nc['add_net'].append(Sexp('add_net',net[1]))
             except FileNotFoundError:
                 print(f"Warrning: kicad_pcb.py detected a kicad v6 file format, but\
                     \n could not find {circuitname}.kicad_pro in the directory of {circuitname}.kicad_pcb file\
@@ -108,9 +121,23 @@ class KicadPCB(SexpParser):
         
         return ret
 
-    def V6ToV5Naming(self):
-        for net_class in self.net_class[0]:     
-            net_class['via_dia'] = net_class['via_diameter']
-            net_class['trace_width'] = net_class['wire_width']
-            del net_class['wire_width']
-            del net_class['via_diameter']
+CLASSED_NETS = set()
+
+def V6ToV5Naming(dict):
+    dict = collections.OrderedDict(dict)
+    dict[0] = dict['name']
+    dict['via_dia'] = dict['via_diameter']
+    dict['trace_width'] = dict['wire_width']
+    del dict['wire_width']
+    del dict['via_diameter']
+
+
+    dict['add_net'] = list()    # The v5 net-to-class structure the Sexp: (net_class {name} {desc} (add_net {name})...)
+                                # which converts to a List of add_net Sexp Objs with {name} values
+    for k in dict.keys():
+        if k == "nets":
+            for item in dict[k]:
+                dict["add_net"].append(Sexp("add_net", f'"{item}"'))    # Quotes preserved for Sexp
+                CLASSED_NETS.add(item)
+        dict[k] = Sexp(k, dict[k])
+    return dict
